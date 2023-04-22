f017ee635ffb5126aaddc3e0990b4f394c1a46c2
SaYMooN <shokoladnysaymoon@mail.ru>
2023-04-22 22:43:58 +0300

    Interface upading

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using MDE_2.Code;
 using System;
+using System.Data;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -17,13 +18,29 @@ namespace MDE_2
         private void WinStarting()
         {
             Reporter.CreateFile();
+            SizeChanged += UIElementsInstalling;
+            StateChanged += UIElementsInstalling;
+            UIElementsInstalling(new object(), null);
             Closing += WinClosing;
             Settings.Deserialize();
             themeCollection = new ThemeCollection();
             Reporter.Log(themeCollection.ToString());
-            SizeChanged += UIElementsInstalling;
+
             ColorTheElements();
         }
+
+        private void UIElementsInstalling(object? sender, EventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                Width = SystemParameters.WorkArea.Width;
+                Height = SystemParameters.WorkArea.Height;
+                UIElementsInstalling(new object(), null);
+            }
+
+
+        }
+
         private void WinClosing(object? sender, System.ComponentModel.CancelEventArgs e)
         {
             themeCollection.SerializeThemeList();
@@ -31,15 +48,24 @@ namespace MDE_2
         }
         private void UIElementsInstalling(object sender, SizeChangedEventArgs e)
         {
-            SetElementSize(CenterGrid, 0.2, 0.38);
-            SetElementOnPosition(CenterGrid, 0.5, 0);
+
+            SetElementSize(MainPage, 1, 1);
+            SetElementSize(CenterGrid, 0.24, 0.44);
+            SetElementOnPosition(CenterGrid, 0.64, 0);
+            SetMarginForElementsInGrid(CenterGrid, 0.1);
+            SetButtonBorderSize(DevelopBTN);
+            SetButtonFontSize(DevelopBTN,5);
+            SetButtonBorderSize(HistoryBTN);
+            SetButtonFontSize(HistoryBTN, 5);
+            SetButtonBorderSize(SettinsBTN);
+            SetButtonFontSize(SettinsBTN,5);
+
         }
-        private void CenterElement(FrameworkElement control) { SetElementOnPosition(control, 0, 0); }
-        private void SetElementOnPosition(FrameworkElement control, double verticalMargin, double gorizontalMargin)
+        private void SetElementOnPosition(FrameworkElement control, double verticalMargin, double horizontalMargin)
         {
             try
             {
-                Canvas.SetLeft(control, (Width - control.Width) / 2 * (gorizontalMargin + 1));
+                Canvas.SetLeft(control, (Width - control.Width) / 2 * (horizontalMargin + 1));
                 Canvas.SetTop(control, (Height - control.Height) / 2 * (verticalMargin + 1));
 
             }
@@ -48,6 +74,7 @@ namespace MDE_2
                 Reporter.Log("ERROR: SetElement \n" + ex.Message);
             }
         }
+        private void SetMarginForElementsInGrid(Grid g, double multiplier) { foreach (Control control in g.Children) control.Margin=new Thickness(multiplier*g.Height*0.2); }
         private void SetElementSize(FrameworkElement control, double widthMultiplier, double heightMultiplier)
         {
             try
@@ -60,6 +87,23 @@ namespace MDE_2
             {
                 Reporter.Log("ERROR: SetElementSize \n" + ex.Message);
             }
+        }
+        private void SetButtonBorderSize(Button control) { SetButtonBorderSize(control, 1); }
+        private void SetButtonFontSize(Button control, double multiplier) {
+            try
+            {
+                control.FontSize = Math.Min(control.ActualWidth/2, control.ActualHeight)/10 * multiplier+2;
+            }
+            catch
+            {
+                Reporter.Log($"Error: SetButtonSize for {control.Name} Heght: {control.Height} Width: {control.Width}");
+                return;
+            }
+        }
+        private void SetButtonBorderSize(Button control, double Multiplier)
+        {
+            Thickness thickness = new Thickness(Math.Sqrt(control.ActualHeight * control.ActualWidth) * 0.05 * Multiplier);
+            control.BorderThickness = thickness;
         }
         private void ColorTheElements()
         {
